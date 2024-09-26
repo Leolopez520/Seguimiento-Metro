ad6fa849e518c90f4781c67fec02feed0ef4cdd4
Hector-O <hectorgarciaoz.hg15@gmail.com>
2024-09-26 00:18:55 -0600

    Inicializacion de front con react y creacion de pagina de login

---
+++ front-tt/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import ForgotPassword from './components/ForgotPassword';
+import ResetPassword from './components/ResetPassword';
+import UserProfile from './components/UserProfile';
+import HomePage from './pages/HomePage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
